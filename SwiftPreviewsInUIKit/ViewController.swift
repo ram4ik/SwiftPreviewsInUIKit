216d4637162d5c6e6f64b6b0f9287a9a00ea0305
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-15 22:35:46 +0300

    Initial Commit

---
+++ SwiftPreviewsInUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPreviewsInUIKit
+//
+//  Created by Ramill Ibragimov on 15.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
